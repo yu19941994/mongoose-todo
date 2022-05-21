deb91d76eab01871d138b9a471d46d0e09e80cfd
yu19941994 <yishian.yu@gmail.com>
2022-05-21 19:24:36 +0800

    fix: change unsuitable data name

--- server.js
+++ server.js
@@ -89,7 +89,7 @@ const requestListener = async (req, res) => {
         res.writeHead(200, headers);
         res.write(JSON.stringify({
             'status': 'success',
-            romms: []
+            posts: []
         }))
         res.end();
     } else if (req.url === '/posts' && req.method === 'OPTIONS') {
