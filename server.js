3e873495e78d9383dd4a43d9ee6fe1646c428b6e
yu19941994 <yishian.yu@gmail.com>
2022-05-21 22:05:19 +0800

    refactor: 將 connections 程式碼拆出

--- server.js
+++ server.js
@@ -1,17 +1,6 @@
 const http = require('http');
-const mongoose = require('mongoose');
 const Post = require('./models/post');
-const dotenv = require('dotenv');
-
-dotenv.config({ path: "./config.env" });
-const DB = process.env.DATABASE.replace(
-    '<password>',
-    process.env.DATABASE_PASSWORD
-)
-
-mongoose.connect(DB)
-.then(() => console.log('connect success'))
-.catch((err) => console.log(err.reason))
+require('./connections');
 
 const requestListener = async (req, res) => {
     let body = '';
