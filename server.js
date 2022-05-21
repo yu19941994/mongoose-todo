c7492c6261e32cdc2382b5c40c960f3ae3d98ea9
yu19941994 <yishian.yu@gmail.com>
2022-05-21 18:21:45 +0800

    feat: add headers

--- server.js
+++ server.js
@@ -10,6 +10,12 @@ const requestListener = async (req, res) => {
     req.on('data', chunk => {
         body += chunk;
     })
+    const headers = {
+        'Access-Control-Allow-Headers': 'Content-Type, Authorization, Content-Length, X-Requested-With',
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'PATCH, POST, GET, OPTIONS, DELETE',
+        'Content-Type': 'application/json'
+    }
 }
 
 const server = http.createServer(requestListener);
